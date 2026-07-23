5c834bd58d1f50ada835ed1ff2e3ee9798bc3628
etherforge2 <aetherforge2.0@gmail.com>
2026-07-23 09:48:52 -0500

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -39,7 +39,7 @@ const SOUPS = [
   price: 8500, 
   from: "#4E8F72", 
   to: "#1F4A3B",
-  img: "https://share.google/Tv4OarQwmUpzxxGao", 
+  img: "https://i.imgur.com/8vKzL2P.jpeg",
   desc: "A vibrant, stretchy okra stew loaded with prawns, crab and the day's fresh catch." 
 },
   { id: "afang", name: "Afang Soup", initial: "A", price: 9000, from: "#5C8A3E", to: "#233F16",
