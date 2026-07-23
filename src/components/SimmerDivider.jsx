1d5794d4f83a8b7f136df47e5fa9a2e8395ae7ba
etherforge2 <aetherforge2.0@gmail.com>
2026-07-23 08:25:39 -0500

    Create SimmerDivider.jsx

---
+++ src/components/SimmerDivider.jsx
@@ -0,0 +1,11 @@
+export default function SimmerDivider() {
+  return (
+    <div className="py-12 flex justify-center">
+      <div className="flex items-center gap-3 text-orange-600">
+        <div className="h-px w-12 bg-orange-600"></div>
+        <span className="text-2xl">♨︎</span>
+        <div className="h-px w-12 bg-orange-600"></div>
+      </div>
+    </div>
+  );
+}
